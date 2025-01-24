94f4ee84a9fa9a09fdadf293cbb4fd7566f0b5bc
Preeti Jaiswal <prepreetijaiswal@gmail.com>
2025-01-24 20:53:24 +0530

    Orderdetails Table Added

--- src/customHooks/UseFetchDocument.js
+++ src/customHooks/UseFetchDocument.js
@@ -1,16 +1,18 @@
-import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase/config";
+import { doc, getDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const useFetchDocument = (collectionName, documentID) =>{
+    console.log("Entry");
+
     const [document, setDocument] = useState(null);
     const getDocument = async () =>{
         const docRef = doc(db, collectionName, documentID);
         const docSnap = await getDoc(docRef);
     
         if (docSnap.exists()) {
-          //console.log("Document data:", docSnap.data());
+          console.log("Document data:", docSnap.data());
           const obj ={
             id: documentID,
             ...docSnap.data(),
