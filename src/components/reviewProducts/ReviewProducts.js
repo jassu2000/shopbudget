2f9b8f5c2de158dd143f00637ee8b886319b0ad4
Preeti Jaiswal <prepreetijaiswal@gmail.com>
2025-01-24 23:28:31 +0530

    Added product review page and collection reviews to firebase

--- src/components/reviewProducts/ReviewProducts.js
+++ src/components/reviewProducts/ReviewProducts.js
@@ -1,11 +1,79 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import styles from "./ReviewProducts.module.scss";
+import { useSelector } from 'react-redux';
+import { selectUserID, selectUserName } from '../../redux/slice/authSlice';
+import { selectProducts } from '../../redux/slice/productSlice';
+import { useParams } from 'react-router-dom';
+import Card from '../card/Card';
+import StarsRating from 'react-star-rate';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import { db } from '../../firebase/config';
+import { toast } from 'react-toastify';
+import useFetchDocument from '../../customHooks/UseFetchDocument';
+import spinnerImg from "../../assets/spinner.jpg";
 const ReviewProducts = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [rate, setRate] = useState(0);
+  const [review, setReview] = useState("");
+  const [product, setProduct] = useState(null);
 
-export default ReviewProducts
+  const {id} = useParams();
+  const {document} = useFetchDocument("products", id);
+  // const products = useSelector(selectProducts);
+  const userID = useSelector(selectUserID);
+  const userName = useSelector(selectUserName);
+  useEffect(()=>{
+    setProduct(document);
+  },[document]);
+  const submitReview = (e) =>{
+    e.preventDefault();
+    const today = new Date();
+        const date = today.toDateString();
+        const reviewConfig = {
+          userID,
+          userName,
+          productID: id,
+          rate,
+          review,
+          reviewDate: date,
+          createdAt: Timestamp.now().toDate()
+        }
+        try{
+          addDoc(collection(db, "reviews"), reviewConfig);
+          toast.success(" Review submitted");
+          setRate(0);
+          setReview("");
+        }
+        catch(error){
+          toast.error(error.message)
+        }
+  }
+  return (
+    <section>
+      <div className={`container ${styles.review}`}>
+          <h2>Rate This Product</h2>
+          {product===null ? (
+            <img src={spinnerImg} alt='Loading....' style={{width:"50px"}} />
+          ) :(
+          <>
+          <p>
+            <b>Product Name: </b> {product.name}
+          </p>
+          <img src={product.imageURL} alt={product.name} style={{width:"100px"}} />
+          </>
+        )}
+          <Card cardClass={styles.card}>
+            <form onSubmit={(e) => submitReview(e)}>
+              <label>Rating:</label>
+              <StarsRating value={rate} onChange={rate => {setRate(rate)}} />
+                <label>Review: </label>
+                <textarea value={review} required onChange={(e) => setReview(e.target.value)} cols="30" rows="10" />
+                <button type='submit' className='--btn --btn-primary'>Submit Review</button>
+            </form>
+          </Card>
+        
+      </div>
+    </section>
+  );
+};
+
+export default ReviewProducts;
