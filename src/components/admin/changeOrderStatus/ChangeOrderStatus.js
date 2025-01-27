be03ca67b1143e5eae85c572bfabc6b7a73ce92f
Preeti Jaiswal <prepreetijaiswal@gmail.com>
2025-01-27 22:34:45 +0530

    Save Order status in Firebase

--- src/components/admin/changeOrderStatus/ChangeOrderStatus.js
+++ src/components/admin/changeOrderStatus/ChangeOrderStatus.js
@@ -2,21 +2,49 @@ import React, { useState } from 'react'
 import styles from "./ChangeOrderStatus.module.scss"
 import Card from '../../card/Card'
 import Loader from '../../loader/Loader';
-const ChangeOrderStatus = () => {
+import { collection, doc, setDoc, Timestamp } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { db } from '../../../firebase/config';
+import { toast } from 'react-toastify';
 
+const ChangeOrderStatus = ({order,id}) => {
     const [status, setStatus] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+    const editOrder = (e,id) =>{
+        e.preventDefault();
+        setIsLoading(true);
+        const orderConfig = {
+          userId:order.userId,
+          userEmail: order.userEmail,
+          orderDate: order.orderDate,
+          orderTime: order.orderTime,
+          orderAmount: order.orderAmount,
+          orderStatus: status,
+          cartItems: order.cartItems,
+          shippingAddress:order.shippingAddress,
+          createdAt:order.createdAt,
+          editedAt:Timestamp.now().toDate(),
+        };
+        try{
+          setDoc(doc(db, "orders", id), orderConfig);
+          setIsLoading(false);
+          toast.success("order Status Updated Successfully");
+          navigate("/admin/orders")
+        }
+        catch(error){
+          setIsLoading(false);
+          toast.error(error.message)
+        }
+      }
 
-    const editOrder = () =>{
-
-    }
   return (
       <>
       {isLoading ? <Loader /> : (
         <div className={styles.status}>
       <Card cardClass={styles.card}>
         <h4>Update Order Status</h4>
-        <form onSubmit={editOrder}>
+        <form onSubmit={(e) => editOrder(e,id)}>
             <span>
                 <select value={status} onChange={(e) => setStatus(e.target.value)}>
                     <option value="">---Select---</option>
